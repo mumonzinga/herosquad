14702303b1094c41eda6f01a1057fe4b10c1f335
Mumo Nzinga <mnzinga888@gmail.com>
2019-05-01 03:47:21 +0300

    test getage

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -6,13 +6,19 @@ public class HeroTest {
 
     @Test
     public void Hero_instantiatesCorrectly_true() {
-        main.java.Hero myHero = new main.java.Hero("mumo", 65, "fire", "liar");
+        main.java.Hero myHero = new main.java.Hero("mumo", 40, "fire", "liar");
         assertEquals(true, myHero instanceof main.java.Hero);
     }
     @Test
     public void getName_forHeroesName_String() {
-        main.java.Hero myHero = new main.java.Hero("mumo",65,"fire","liar");
+        main.java.Hero myHero = new main.java.Hero("mumo",40,"fire","liar");
         String expected = "mumo";
         assertEquals(expected, myHero.getName());
+    }
+    @Test
+    public void getAge_forHeroesName_String() {
+        main.java.Hero myHero = new main.java.Hero("mumo",40,"fire","liar");
+        int expected = 40;
+        assertEquals(expected, myHero.getAge());
     }
 }
