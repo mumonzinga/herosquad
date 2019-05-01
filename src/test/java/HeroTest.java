a8df633dc68b4cbfdb87a613bda0d5a4f4f7bb59
Mumo Nzinga <mnzinga888@gmail.com>
2019-05-01 03:45:36 +0300

    test getname

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -6,7 +6,13 @@ public class HeroTest {
 
     @Test
     public void Hero_instantiatesCorrectly_true() {
-        main.java.Hero myHero = new main.java.Hero("Vick", 65, "madem", "madem");
+        main.java.Hero myHero = new main.java.Hero("mumo", 65, "fire", "liar");
         assertEquals(true, myHero instanceof main.java.Hero);
+    }
+    @Test
+    public void getName_forHeroesName_String() {
+        main.java.Hero myHero = new main.java.Hero("mumo",65,"fire","liar");
+        String expected = "mumo";
+        assertEquals(expected, myHero.getName());
     }
 }
