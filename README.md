450b15adcc016bfd95c248961b36e4d186ae6336
Mumo Nzinga <mnzinga888@gmail.com>
2019-05-02 10:55:25 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,46 @@
+#  Hero-Squad
+>*[GitHub Stars](https://github.com/mumonzinga/hero-squad/stargazers) *[GitHub Issues](https://github.com/mumonzinga/ceasercipher/issues)
+
+#### An application that easens your search for heroes and squads, 02/2019
+
+#### By
+**[Mumo Nzinga](https://github.com/mumonzinga)**
+
+### Description
+
+This application allows USERS to add heroes and recruit them in a squad.
+ ###User Requirements
+
+This application allows users to do the following:
+1.Add Heroes
+2.Add/recruit a squad.
+3.State the cause of the squad.
+
+
+## Project setup / requirements
+1.Make sure you have all the Requirements of running Java apps installed such as JUnit, intellij, SDK, JDK.
+
+2.Clone the project into your machine from https://github.com/mumonzinga/hero-squad.git
+
+3.Run javac hero-squad.java to compile and java hero-squad to run the program
+
+## Technologies Used
+
+-JDK
+-Junit
+-Java
+-SDK
+
+## Support and contact details
+If you have any inquiries, comments; Contact me at mnzinga888@gmail.com
+
+
+### License and Copyright details
+>You can check the full license [here](https://github.com/mumonzinga/LICENSE.git)
+
+This project is licensed under the terms of the **GNU GPLv3** license.
+
+
+
+
+
--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -6,70 +6,70 @@ public class SquadTest {
 
     @Test
     public void squad_instantiatesCorrectly_true() {
-        Squad testSquad = new Squad("nerdsClub", 5, "Computer illiteracy");
+        Squad testSquad = new Squad("brightclub", 7, "creativity");
         assertEquals(true, testSquad instanceof Squad);
     }
     @Test
     public void getName_squadInstantiatesWithName_String() {
-        Squad testSquad = new Squad("nerdsClub", 5, "Computer illiteracy");
-        assertEquals("nerdsClub", testSquad.getName());
+        Squad testSquad = new Squad("brightclub", 7, "creativity");
+        assertEquals("brightclub", testSquad.getName());
     }
     @Test
     public void getSize_squadInstantiatesWithSize_int() {
-        Squad testSquad = new Squad("nerdsClubs", 5, "Computer illiteracy");
-        assertEquals(5, testSquad.getSize());
+        Squad testSquad = new Squad("brightclubs", 7, "creativity");
+        assertEquals(7, testSquad.getSize());
     }
     @Test
     public void getCause_squadInstantiatesWithCause_String() {
-        Squad testSquad = new Squad("nerdsClubs", 5, "Computer illiteracy");
-        assertEquals("nerdsClubs", testSquad.getCause());
+        Squad testSquad = new Squad("brightclubs", 7, "creativity");
+        assertEquals("creativity", testSquad.getCause());
     }
     @Test
     public void all_returnsAllInstancesOfSquad_true() {
-        Squad firstSquad = new Squad("nerdsClubs", 5, "Computer illiteracy");
-        Squad secondSquad = new Squad("Geeks", 4, "Java literacy");
+        Squad firstSquad = new Squad("brightclubs", 7, "creativity");
+        Squad secondSquad = new Squad("army", 4, "strong soldiers");
         assertEquals(true, Squad.all().contains(firstSquad));
         assertEquals(true, Squad.all().contains(secondSquad));
     }
     @Test
     public void clear_emptiesAllSquadsFromList_0() {
-        Squad testSquad = new Squad("nerdsClubs", 5, "Computer illiteracy");
+        Squad testSquad = new Squad("brightclubs", 7, "creativity");
         Squad.clear();
         assertEquals(Squad.all().size(), 0);
     }
     @Test
     public void getId_squadsInstantiatesWithAnId_1() {
         Squad.clear();
-        Squad testSquad = new Squad("nerdsClubs", 5, "Computer illiteracy");
+        Squad testSquad = new Squad("brightclubs", 7, "creativity");
         assertEquals(1, testSquad.getId());
     }
 
     @Test
     public void find_returnsSquadWithSameId_secondSquad() {
         Squad.clear();
-        Squad firstSquad = new Squad("nerdsClubs", 5, "Computer illiteracy");
-        Squad secondSquad = new Squad("Geeks", 4, "Java literacy ");
+        Squad firstSquad = new Squad("brightclubs", 7, "creativity");
+        Squad secondSquad = new Squad("army", 4, "strong soldiers ");
         assertEquals(Squad.find(secondSquad.getId()), secondSquad);
     }
 
     @Test
     public void getHeroes_initiallyReturnsEmptyList_ArrayList() {
         Squad.clear();
-        Squad testSquad = new Squad("nerdsClubs", 5, "Computer iliteracy");
+        Squad testSquad = new Squad("brightclubs", 7, "creativity");
         assertEquals(0, testSquad.getHeroes().size());
     }
     @Test
     public void addHero_addsHeroToList_true() {
-        Squad testSquad = new Squad("nerdsClubs", 5, "Computer illiteracy");
-        main.java.Hero testHero = new main.java.Hero("prodigy", 20, "intelligent", "Power failure");
+        Squad testSquad = new Squad("brightclubs", 7, "creativity");
+        main.java.Hero testHero = new main.java.Hero("panther", 20, "intelligent", "anger stricken");
         testSquad.addHero(testHero);
         assertTrue(testSquad.getHeroes().contains(testHero));
     }
 
     @Test
     public void heroAlreadyExists_checksIfHeroExistsinSquads_true() {
-        Squad testSquad = new Squad("nerdsClubs", 5, "Computer illiteracy");
-        main.java.Hero testHero = new main.java.Hero("prodigy", 20, "intelligent", "power outage");
+        Squad testSquad = new Squad("brightclubs", 7, "creativity");
+        main.java.Hero testHero = new main.java.Hero("panther", 20, "intelligent", "anger stricken");
         testSquad.addHero(testHero);
         assertTrue(testSquad.heroAlreadyExists(testHero));
 
